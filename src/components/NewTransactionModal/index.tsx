1f2bc86f1def478edc4b761cf2c63bfa84b92ff2
alessandroibes <alessandro.ibes@gmail.com>
2024-07-06 17:36:45 -0300

    Performance: Aplicando Context Selectors

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -11,7 +11,8 @@ import {
 } from './styles';
 import { Controller, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useContext } from 'react';
+import { useContextSelector } from 'use-context-selector';
+
 import { TransactionsContext } from '../../contexts/TransactionsContext';
 
 const newTransactionFormSchema = z.object({
@@ -24,7 +25,9 @@ const newTransactionFormSchema = z.object({
 type newTransactionFormInputs = z.infer<typeof newTransactionFormSchema>;
 
 export function NewTransactionModal() {
-  const { createTransaction } = useContext(TransactionsContext);
+  const createTransaction = useContextSelector(TransactionsContext, context => {
+    return context.createTransaction;
+  });
 
   const {
     control,
--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -4,7 +4,8 @@ import * as z from 'zod';
 
 import { SearchFormContainer } from './styles';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useContext } from 'react';
+import { useContextSelector } from 'use-context-selector';
+
 import { TransactionsContext } from '../../../../contexts/TransactionsContext';
 
 const searchFormSchema = z.object({
@@ -14,7 +15,9 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
 export function SearchForm() {
-  const { fetchTransactions } = useContext(TransactionsContext);
+  const fetchTransactions = useContextSelector(TransactionsContext, context => {
+    return context.fetchTransactions;
+  });
 
   const {
     register,
